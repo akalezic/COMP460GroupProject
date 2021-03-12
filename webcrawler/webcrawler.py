b85f0617b45360ada60eb72f8825dc30972c904b
Amela Kalezic <amelakalezic@gmail.com>
2021-03-12 16:01:24 -0600

    Initial web crawler work

--- webcrawler/webcrawler.py
+++ webcrawler/webcrawler.py
@@ -1,1 +1,14 @@
-#Pyhton file where webcrawler will be put to scarp the internet for search engine
+#Python file where webcrawler will be put to scrape the internet for search engine
+import requests
+from bs4 import BeautifulSoup
+
+#get url of website you want to scrape
+url = 'https://www.cnn.com/'
+# uses requests to get website
+page = requests.get(url)
+
+soup = BeautifulSoup(page.content, 'lxml')
+#currently struggling to get this to work
+article = soup.find('h3')
+headline = article.span.text
+print(headline)
