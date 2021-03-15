19f652b31f6f1e50cb65dcad3827a4b47924be6f
AdityaDhungana1 <adhungana1@luc.edu>
2021-03-15 15:10:08 +0545

    updated so that text is extracted from html and saved in a folder

--- webcrawler/webcrawler.py
+++ webcrawler/webcrawler.py
@@ -1,14 +1,18 @@
-#Python file where webcrawler will be put to scrape the internet for search engine
+# Python file where webcrawler will be put to scrape the internet for search engine
 import logging
 from urllib.parse import urljoin
 import requests
 from bs4 import BeautifulSoup
+import io
+import os
 
 logging.basicConfig(
     format='%(asctime)s %(levelname)s:%(message)s',
     level=logging.INFO)
 
+
 class Crawler:
+    count = 0
 
     def __init__(self, urls=[]):
         self.visited_urls = []
@@ -30,9 +34,41 @@ class Crawler:
             self.urls_to_visit.append(url)
 
     def crawl(self, url):
-        html = self.download_url(url)
-        for url in self.get_linked_urls(url, html):
-            self.add_url_to_visit(url)
+        # count increments every new page is crawled
+        self.count += 1
+        # it is set to less than 10 so only 10 pages are downloaded
+        if self.count < 12:
+            # extracting only the domain for setting it to the file name
+            # as file name cannot contain characters line '/' and '?'
+            html = self.download_url(url)
+            url_domain = url[8:]
+            save_path = "./crawledPages"
+            url_domain = url_domain.replace('?', '')
+            domain = []
+            domain = url_domain.split('/')
+
+            # extracting text from html
+            soup = BeautifulSoup(html)
+            for script in soup(["script", "style"]):
+                script.decompose()
+
+            strips = list(soup.stripped_strings)
+
+            # creating a file for the crawled page
+            if domain[1] == "":
+                file_name = domain[0] + '.txt'
+            else:
+                file_name = domain[0] + "-" + domain[1] + '.txt'
+
+            complete_name = os.path.join(save_path, file_name)
+            with io.open(complete_name, "w", encoding="utf-8") as f:
+                for text in strips:
+                    f.write(text)
+                f.close()
+
+            # finding and adding urls in the crawled page
+            for url in self.get_linked_urls(url, html):
+                self.add_url_to_visit(url)
 
     def run(self):
         while self.urls_to_visit:
@@ -44,6 +80,7 @@ class Crawler:
                 logging.exception(f'Failed to crawl: {url}')
             finally:
                 self.visited_urls.append(url)
+
 
 if __name__ == '__main__':
     Crawler(urls=['https://www.cnn.com/']).run()
