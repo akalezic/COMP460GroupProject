fd2f8c4fdba64f4f2ab804f8053dc3ffb9fbda0d
jakobveselsky <jveselsky@luc.edu>
2021-03-16 17:20:50 -0500

    addition of flask ui

---
+++ Web Client/flask/flaskDemo/flaskDemo.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template, url_for, flash, redirect
+from flaskDemo.forms import RegistrationForm, LoginForm, SearchForm
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+
+results = list()
+result = dict()
+result['title']="War"
+result['description'] = "blah blah blah blah"
+results.append(result)
+result=dict()
+result['title']="Peace"
+result['description'] = "blah blah blah blah"
+results.append(result)
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', results = results)
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+if __name__ == '__main__':
+    app.run(debug=True)
